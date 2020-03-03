794a398175631049661f2b544f29a15050da282a
David A <josedavac@gmail.com>
2020-03-03 14:36:32 -0600

    Adding screenshot of home page. Simplified API call for news scraper

--- scrape_mars.py
+++ scrape_mars.py
@@ -4,48 +4,24 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from pprint import pprint
 import re
-from selenium import webdriver
+import json
 
 def scrape():
 
+	# Main Dict for json string
 	mars_scrapped = {}
 
-
-	# NASA Mars News
-	# nasa_news_r = requests.get("https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest").text
-	# soup = BeautifulSoup(nasa_news_r, 'html.parser')
-	# news_title = soup.find_all('div', class_="content_title")[0].text.replace("\n","")
-	# news_p = soup.find_all('div', class_="rollover_description_inner")[0].text.replace("\n","")
-
-	# Using Selenium for better/more updated results
-	BASE_URL = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-	FILE = "html-selenium.txt"
-	FILE_WAIT = "html-selenium-wait.txt"
-
-	driver = webdriver.Firefox()
-	driver.get(BASE_URL)
-	html = driver.page_source
-
-	with open(FILE, "w+", encoding="utf-8") as f:
-		f.write(html)
-
-	driver.get(BASE_URL)
-	html = driver.page_source
-	driver.implicitly_wait(15)
-	driver.close()
-
-	with open(FILE_WAIT, "w+", encoding="utf-8") as f:
-		f.write(html)
-
-	soup = BeautifulSoup(html, "html.parser")
-	news_title = soup.find_all('div', class_="content_title")[0].text.replace("\n","")
-	news_p = soup.find_all('div', class_="article_teaser_body")[0].text.replace("\n","")
+	### Nasa News Scraper ###
+	nasa_news_r = requests.get("https://mars.nasa.gov/api/v1/news_items/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest")
+	data = nasa_news_r.json()
+	news_title = data['items'][0]['title']
+	news_p = data['items'][0]['description']
 
 	mars_scrapped["mars_news_title"] = news_title
 	mars_scrapped["mars_news_p"] = news_p
 
 
-	# Space Images - Featured Image
+	### Featured Image Scraper ###
 	jpl_mars_r = requests.get("https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars").text
 	soup = BeautifulSoup(jpl_mars_r, 'html.parser')
 
@@ -54,7 +30,7 @@ def scrape():
 	mars_scrapped["featured_img"] = feaured_image_url
 
 
-	# Mars Weather
+	### Mars Weather Scraper ###
 	mars_weather_r = requests.get("https://twitter.com/marswxreport?lang=en").text
 	soup = BeautifulSoup(mars_weather_r, 'html.parser')
 
@@ -63,14 +39,14 @@ def scrape():
 	mars_scrapped["mars_weather"] = mars_weather
 
 
-	# Mars Facts
+	### Mars Facts Scraper ###
 	mars_facts_df = pd.read_html("http://space-facts.com/mars/")[0].rename(columns={0:"Description", 1:"Values"})
 	mars_facts = mars_facts_df.to_html(index=False, header=True, classes="table")
 
 	mars_scrapped["mars_facts"] = mars_facts
 
 
-	# Mars Hemispheres
+	### Mars Hemispheres Scraper ###
 	mars_hemispheres_r = requests.get("https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars").text
 	soup = BeautifulSoup(mars_hemispheres_r, 'html.parser')
 
@@ -78,5 +54,6 @@ def scrape():
 	
 	mars_scrapped["mars_hemispheres"] = hemispheres
 
-	### Printing dict ###
+
+	# Printing dict #
 	return mars_scrapped

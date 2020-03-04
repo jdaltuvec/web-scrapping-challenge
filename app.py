ad5c82633bba3fdfdb586636f32a1acbad4872f1
David A <josedavac@gmail.com>
2020-03-03 18:21:32 -0600

    Intalled black. Reformatted files using black

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pprint import pprint
 app = Flask(__name__)
 
 # Create connection variable
-conn = 'mongodb://localhost:27017'
+conn = "mongodb://localhost:27017"
 
 # Pass connection to the pymongo instance.
 client = pymongo.MongoClient(conn)
@@ -18,23 +18,23 @@ db = client.mars_db
 
 
 # Set route to scrape
-@app.route('/scrape')
+@app.route("/scrape")
 def scrape():
     # Drops collection if available to remove duplicates
     db.mars.drop()
-    
+
     s = scrape_mars.scrape()
     db.mars.insert(s)
     return redirect("/", code=302)
 
 
 # Set route
-@app.route('/')
+@app.route("/")
 def index():
     mars_data = list(db.mars.find())
     print(mars_data)
     # Return the template with the list passed in
-    return render_template('index.html', mars_data=mars_data)
+    return render_template("index.html", mars_data=mars_data)
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -6,54 +6,75 @@ from pprint import pprint
 import re
 import json
 
+
 def scrape():
 
-	# Main Dict for json string
-	mars_scrapped = {}
+    # Main Dict for json string
+    mars_scrapped = {}
 
-	### Nasa News Scraper ###
-	nasa_news_r = requests.get("https://mars.nasa.gov/api/v1/news_items/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest")
-	data = nasa_news_r.json()
-	news_title = data['items'][0]['title']
-	news_p = data['items'][0]['description']
+    ### Nasa News Scraper ###
+    nasa_news_r = requests.get(
+        "https://mars.nasa.gov/api/v1/news_items/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    )
+    data = nasa_news_r.json()
+    news_title = data["items"][0]["title"]
+    news_p = data["items"][0]["description"]
 
-	mars_scrapped["mars_news_title"] = news_title
-	mars_scrapped["mars_news_p"] = news_p
+    mars_scrapped["mars_news_title"] = news_title
+    mars_scrapped["mars_news_p"] = news_p
 
+    ### Featured Image Scraper ###
+    jpl_mars_r = requests.get(
+        "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    ).text
+    soup = BeautifulSoup(jpl_mars_r, "html.parser")
 
-	### Featured Image Scraper ###
-	jpl_mars_r = requests.get("https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars").text
-	soup = BeautifulSoup(jpl_mars_r, 'html.parser')
+    feaured_image_url = (
+        "https://www.jpl.nasa.gov"
+        + soup.find_all("a", class_="fancybox")[0]["data-fancybox-href"]
+    )
 
-	feaured_image_url = "https://www.jpl.nasa.gov" + soup.find_all('a', class_="fancybox")[0]["data-fancybox-href"]
+    mars_scrapped["featured_img"] = feaured_image_url
 
-	mars_scrapped["featured_img"] = feaured_image_url
+    ### Mars Weather Scraper ###
+    mars_weather_r = requests.get("https://twitter.com/marswxreport?lang=en").text
+    soup = BeautifulSoup(mars_weather_r, "html.parser")
 
+    mars_weather = soup.find_all(
+        "p", class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"
+    )[0].text.replace("\n", " ")[:-26]
+    mars_weather = re.sub(r"[^ ]+\.[^ ]+", "", mars_weather)
+    mars_scrapped["mars_weather"] = mars_weather
 
-	### Mars Weather Scraper ###
-	mars_weather_r = requests.get("https://twitter.com/marswxreport?lang=en").text
-	soup = BeautifulSoup(mars_weather_r, 'html.parser')
+    ### Mars Facts Scraper ###
+    mars_facts_df = pd.read_html("http://space-facts.com/mars/")[0].rename(
+        columns={0: "Description", 1: "Values"}
+    )
+    mars_facts = mars_facts_df.to_html(index=False, header=True, classes="table")
 
-	mars_weather = soup.find_all('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")[0].text.replace("\n"," ")[:-26]
-	mars_weather = re.sub(r'[^ ]+\.[^ ]+', '', mars_weather)
-	mars_scrapped["mars_weather"] = mars_weather
+    mars_scrapped["mars_facts"] = mars_facts
 
+    ### Mars Hemispheres Scraper ###
+    mars_hemispheres_r = requests.get(
+        "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    ).text
+    soup = BeautifulSoup(mars_hemispheres_r, "html.parser")
 
-	### Mars Facts Scraper ###
-	mars_facts_df = pd.read_html("http://space-facts.com/mars/")[0].rename(columns={0:"Description", 1:"Values"})
-	mars_facts = mars_facts_df.to_html(index=False, header=True, classes="table")
+    hemispheres = [
+        {
+            "title": e.text.strip(" Enhanced"),
+            "img_url": (
+                "https://"
+                + "astropedia."
+                + "astrogeology.usgs.gov"
+                + e["href"]
+                + ".tif/full.jpg"
+            ).replace("search/map", "download"),
+        }
+        for e in soup.find_all(class_="itemLink product-item")
+    ]
 
-	mars_scrapped["mars_facts"] = mars_facts
+    mars_scrapped["mars_hemispheres"] = hemispheres
 
-
-	### Mars Hemispheres Scraper ###
-	mars_hemispheres_r = requests.get("https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars").text
-	soup = BeautifulSoup(mars_hemispheres_r, 'html.parser')
-
-	hemispheres = [{'title': e.text.strip(' Enhanced'), 'img_url':("https://" + "astropedia." + "astrogeology.usgs.gov" + e['href'] + '.tif/full.jpg').replace("search/map", "download")} for e in soup.find_all(class_="itemLink product-item")]
-	
-	mars_scrapped["mars_hemispheres"] = hemispheres
-
-
-	# Printing dict #
-	return mars_scrapped
+    # Printing dict #
+    return mars_scrapped
